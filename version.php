117667f584b9559fe0a3b7ebe919897013f04f3b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-07-08 19:36:45 -0300

    upd(version): atualizar versão do plugin para 0.1.03

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_recommendation';
 $plugin->release = '0.1.03';
-$plugin->version = 2025070302;
+$plugin->version = 2025070303;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
